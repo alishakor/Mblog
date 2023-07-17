b5024c9f8b2b339e3ff30ace104322d7dc25b061
alishakor <113800450+alishakor@users.noreply.github.com>
2023-07-17 16:10:15 +0100

    changed url route

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-
+from flask import render_template
 from app import app
 
-@app.route('/')
+# @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello! universe"
+    user = {'username': "Sterling"}
+    return render_template('index.html', title='Home', user=user)
